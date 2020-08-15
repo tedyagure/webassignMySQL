0224d0ab759ede303e392bb75523c42b52ffabf5
tedyagure <agure84ngochi@gmail.com>
2020-08-15 17:49:55 +0530

    add function

--- server.js
+++ server.js
@@ -12,16 +12,45 @@ const app = express();
 const { User } = require("./db");
 
 app.get("/", (req, res) =>
-    res.send("Welcome to Node.js + MySQL boilerplate API.")
+  res.send("Welcome to Node.js + MySQL boilerplate API.")
 );
 
 app.get("/users", (req, res) => {
-    User.findAll().then((users) => {
-        res.send(users);
-    });
+  User.findAll().then((users) => {
+    res.send(users);
+  });
+});
+
+app.post("/file/upload", uploadStrategy, (req, res) => {
+  try {
+    axios
+      .post("https://tedyfunc.azurewebsites.net/api/HttpTrigger1", {
+        filedata: req.file.buffer,
+        filename: req.file.originalname,
+      })
+      .then((Res) => {
+        if (Res.status === 200) {
+          return res.status(200).json({
+            message: "Image Uploaded!",
+            statusCode: 200,
+          });
+        } else {
+          throw error;
+        }
+      })
+      .catch((e) => {
+        return res.status(200).json({
+          message: "Image Upload failed!",
+          statusCode: 400,
+        });
+        console.error(e);
+      });
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log("Server listening on port:", PORT);
+  console.log("Server listening on port:", PORT);
 });

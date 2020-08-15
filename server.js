9953229b0dc4f0869819eca85f085c053e9922ad
tedyagure <agure84ngochi@gmail.com>
2020-08-15 17:53:32 +0530

    reverse

--- server.js
+++ server.js
@@ -21,35 +21,6 @@ app.get("/users", (req, res) => {
   });
 });
 
-app.post("/file/upload", uploadStrategy, (req, res) => {
-  try {
-    axios
-      .post("https://tedyfunc.azurewebsites.net/api/HttpTrigger1", {
-        filedata: req.file.buffer,
-        filename: req.file.originalname,
-      })
-      .then((Res) => {
-        if (Res.status === 200) {
-          return res.status(200).json({
-            message: "Image Uploaded!",
-            statusCode: 200,
-          });
-        } else {
-          throw error;
-        }
-      })
-      .catch((e) => {
-        return res.status(200).json({
-          message: "Image Upload failed!",
-          statusCode: 400,
-        });
-        console.error(e);
-      });
-  } catch (e) {
-    console.error(e);
-  }
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server listening on port:", PORT);
